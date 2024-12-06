a8b25e188d1e909c5e35d91e0fe9bb89a2d0dc3f
bongbong2da <snk3981@gmail.com>
2024-12-06 18:58:36 +0900

    [Feat] Coin search screen

---
+++ app/(tabs)/(home)/CoinSearchScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text } from "react-native";
+
+const CoinSearchScreen = () => {
+  return (
+    <Container>
+      <Text>CoinSearchScreen</Text>
+    </Container>
+  );
+};
+
+const Container = styled.SafeAreaView``;
+
+export default CoinSearchScreen;
