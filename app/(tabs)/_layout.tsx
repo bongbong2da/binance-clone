e3c2837f0fa78c74a5fcbc75df90c0838374dc26
bongbong2da <snk3981@gmail.com>
2024-12-09 03:22:40 +0900

    [Design] Bottom tab design

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,28 @@ import { Tabs } from "expo-router";
 import React from "react";
 import { useRecoilValue } from "recoil";
 import { tabBarVisibleAtom } from "@/recoil/atoms/UIAtoms";
+import styled from "styled-components/native";
+
+type TabType = "home" | "trades";
 
 const TabLayout = () => {
   const isHide = useRecoilValue(tabBarVisibleAtom);
+
+  const renderTabBarIcon = (props: { focused: boolean }, tab: TabType) => {
+    let source;
+    switch (tab) {
+      case "home":
+        source = require("@/assets/images/icons/home.png");
+        break;
+      case "trades":
+        source = require("@/assets/images/icons/trade.png");
+        break;
+    }
+
+    if (!source) return null;
+
+    return <TabBarIcon source={source} isFocused={props.focused} />;
+  };
 
   return (
     <Tabs initialRouteName={"(home)"}>
@@ -16,6 +35,8 @@ const TabLayout = () => {
           tabBarStyle: {
             display: isHide ? "flex" : "none",
           },
+          tabBarActiveTintColor: "#000",
+          tabBarIcon: (props) => renderTabBarIcon(props, "home"),
         }}
       />
       <Tabs.Screen
@@ -26,10 +47,18 @@ const TabLayout = () => {
           tabBarStyle: {
             display: isHide ? "flex" : "none",
           },
+          tabBarActiveTintColor: "#000",
+          tabBarIcon: (props) => renderTabBarIcon(props, "trades"),
         }}
       />
     </Tabs>
   );
 };
+
+const TabBarIcon = styled.Image<{ isFocused: boolean }>`
+  width: 24px;
+  height: 24px;
+  tint-color: ${({ isFocused }) => (isFocused ? "#000" : "#9f9f9f")};
+`;
 
 export default TabLayout;
